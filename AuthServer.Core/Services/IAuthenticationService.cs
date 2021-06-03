da249e132cdc90138d7805ace0c59f577dd6f3cd
mmustafaseker <mustiseker.06@gmail.com>
2021-06-03 20:21:02 +0300

    Add project files.

---
+++ AuthServer.Core/Services/IAuthenticationService.cs
@@ -0,0 +1,18 @@
+﻿using AuthServer.Core.DTO_s;
+using SharedLibrary.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.Core.Services
+{
+    public interface IAuthenticationService
+    {
+        Task<Response<TokenDto>> CreateToken(LoginDto loginDto);
+        Task<Response<TokenDto>> CreateTokenByRefreshToken(string refresToken);
+        Task<Response<NoDataDto>> RevokeRefreshToken(string refreshToken);
+        Task<Response<ClientTokenDto>> CreateTokenByClient(ClientLoginDto clientLoginDto);
+    }
+}
