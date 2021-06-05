4af5cca7763b8c8d78b58c1fc3e54c92f51fdb77
mmustafaseker <mustiseker.06@gmail.com>
2021-06-05 15:40:12 +0300

    Token Service

--- AuthServer.Core/Repositories/IGenericRepository.cs
+++ AuthServer.Core/Repositories/IGenericRepository.cs
@@ -9,7 +9,7 @@ namespace AuthServer.Core.Repositories
 {
     public interface IGenericRepository<TEntity> where TEntity : class
     {
-        Task<TEntity> GetByIdAsync(int Id);
+        Task<TEntity> GetByIdAsync(int id);
         Task<IEnumerable<TEntity>> GetAllAsync();
         IQueryable<TEntity> Where(Expression<Func<TEntity, bool>> predicate);
         Task AddAsync(TEntity entity);
--- AuthServer.Core/Services/IServiceGeneric.cs
+++ AuthServer.Core/Services/IServiceGeneric.cs
@@ -10,11 +10,11 @@ namespace AuthServer.Core.Services
 {
     public interface IServiceGeneric<TEntity, TDto> where TEntity : class where TDto : class
     {
-        Task<Response<TDto>> GetByIdAsync(int Id);
+        Task<Response<TDto>> GetByIdAsync(int id);
         Task<Response<IEnumerable<TDto>>> GetAllAsync();
         Task<Response<IEnumerable<TDto>>> Where(Expression<Func<TEntity, bool>> predicate);
-        Task<Response<TDto>> AddAsync(TEntity entity);
-        Task<Response<NoDataDto>> Remove(TEntity entity);
-        Task<Response<NoDataDto>> Update(TEntity entity);
+        Task<Response<TDto>> AddAsync(TDto entity);
+        Task<Response<NoDataDto>> Remove(int id);
+        Task<Response<NoDataDto>> Update(TDto entity, int id);
     }
 }
